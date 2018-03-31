c93c32f72399fc29b0bc755d095a6d4f4350e5ae
Victor Jambo <victormutaijambo@gmail.com>
2018-03-31 19:56:29 +0000

    [Fixes] Display navbar links if logged in

--- src/common/NavigationBar.jsx
+++ src/common/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../Auth/Auth';
 
 class NavigationBar extends Component {
   
@@ -19,9 +20,26 @@ class NavigationBar extends Component {
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/">Home</Link></li>
-              <li className="dropdown"><Link to="/auth/signup">Register</Link></li>
-              <li><Link to="/auth/login">login</Link></li>
+              { !Auth.isAuthenticated && <li className="dropdown">
+                <Link to="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span className="glyphicon glyphicon-bell"></span>
+                  <span className="badge"></span>
+                </Link>
+                <ul className="dropdown-menu">
+                  <li><Link to="/">Notification 1</Link></li>
+                  <li role="separator" className="divider"></li>
+                  <li><Link to="/">Notification 2</Link></li>
+                  <li role="separator" className="divider"></li>
+                  <li><Link to="/">Notification 3</Link></li>
+                  <li role="separator" className="divider"></li>
+                  <li><Link to="/">Notification 4</Link></li>
+                  <li role="separator" className="divider"></li>
+                  <li><Link to="/">Notification 5</Link></li>
+                </ul>
+              </li> }
+              { !Auth.isAuthenticated && <li><Link to="/auth/logout">logout</Link></li> }
+              { Auth.isAuthenticated && <li><Link to="/auth/signup">signup</Link></li> }
+              { Auth.isAuthenticated && <li><Link to="/auth/login">login</Link></li> }
             </ul>
           </div>
         </div>
