d0d3e71a7eb7d275aeb0a413785ab9ed4fc615e7
Victor Jambo <victormutaijambo@gmail.com>
2018-04-04 12:04:33 +0000

    [Chore] Unit Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Hero from './common/Hero';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Register from './Auth/Register';
 import Login from './Auth/Login';
@@ -14,25 +13,29 @@ import SearchForm from './Business/SearchForm';
 
 configure({ adapter: new Adapter() });
 
-it('<BrowserRouter />', () => {
-  const div = document.createElement('root');
-  ReactDOM.render(
-    <BrowserRouter>
-  	  <div>
-  	    <NavigationBar />
-        <Route exact path="/" component={App} />
-        <Route path="/auth/signup" component={Register} />
-        <Route path="/auth/login" component={Login} />
-      </div>
-    </BrowserRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<BrowserRouter />', () =>  {
+  it('Test Browser Routes', () => {
+    const div = document.createElement('root');
+    ReactDOM.render(
+      <BrowserRouter>
+    	  <div>
+    	    <NavigationBar />
+          <Route exact path="/" component={App} />
+          <Route path="/auth/signup" component={Register} />
+          <Route path="/auth/login" component={Login} />
+        </div>
+      </BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
 
 describe('<App />', () =>  {
-  it('test ui elements render children', () => {
+  it('Test Render Businesses', () => {
     const app = shallow(<App />);
-    expect(app.contains(<Businesses />)).to.equal(true);
+    expect(app.contains(<Businesses />)).to.be.true;
+  });
 
+  it('Test Render Search Bar', () => {
     const form = shallow(<SearchForm />);
     expect(form.find('form').length).to.equal(1);
   });
