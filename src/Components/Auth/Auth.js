9b1f16930102129c119e692dda8eb715ad1b9f8d
Victor Jambo <victormutaijambo@gmail.com>
2018-04-12 21:20:57 +0000

    [fixes #156323211] Authentication

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,5 +1,24 @@
+import decode from 'jwt-decode';
+
+function isTokenValid() {
+  let token = sessionStorage.getItem('token');
+  if(!token) {
+    return false;
+  }
+  try {
+    let { exp } = decode(token);
+    if (exp < new Date().getTime() / 1000) {
+      sessionStorage.removeItem('token');
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
 const Auth = {
-  isAuthenticated: false,
+  isAuthenticated: isTokenValid(),
   authenticate() {
     this.isAuthenticated = true;
   },
--- src/Components/Business/SearchForm.jsx
+++ src/Components/Business/SearchForm.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import './css/SearchForm.css';
+import decode from 'jwt-decode';
 
 class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e){
+    e.preventDefault();
+    let token = sessionStorage.getItem('token');
+    const { exp } = decode(token);
+    console.log(new Date().getTime() / 1000);
+    console.log(exp);
+  }
+
   render() {
     return (
-      <form method="post" action="">
+      <form onSubmit={this.handleSubmit}>
         <div className="input-group">
           <input type="text" className="form-control" name="search" size="60" placeholder="Business Name, location or category"/>
           <span className="input-group-btn">
