b9304a71534a5b134a18e16d1be40761b744a6fc
Victor Jambo <victormutaijambo@gmail.com>
2018-04-25 18:04:37 +0000

    [Fixes] Fix validation test file to follow eslint

--- src/tests/Validations.test.js
+++ src/tests/Validations.test.js
@@ -2,46 +2,45 @@ import validateInput from '../helpers/validations';
 import { expect } from 'chai';
 
 describe('Validations', () => {
-  let response = 'This field is required';
-  let data = {
+  const response = 'This field is required';
+  const data = {
     username: '',
     password: '',
     fullname: '',
     email: '',
     confirm_password: ''
   };
-    
+
   it('Validates isEmpty', () => {
-    let output = validateInput(data);
-    for(var error in output.errors) {
+    const output = validateInput(data);
+    Object.keys(output.errors).forEach((error) => {
       expect(output.errors[error]).to.equal(response);
-    }
+    });
   });
-  
+
   it('Validates isEmail', () => {
     data.email = 'victor@jambo';
-    let output = validateInput(data);
+    const output = validateInput(data);
     expect(output.errors.email).to.equal('Invalid Email');
   });
-  
+
   it('Validates it matches password', () => {
     data.password = 'password';
-    let output = validateInput(data);
+    const output = validateInput(data);
     expect(output.errors.password).to.equal('Provide strong password, should contain letters and numbers');
   });
-  
+
   it('Validates confirm password', () => {
     data.password = 'password1234';
     data.confirm_password = 'different1234';
-    let output = validateInput(data);
+    const output = validateInput(data);
     expect(output.errors.confirm_password).to.equal('Passwords do not match');
   });
-  
+
   it('Validates same passwords', () => {
     data.password = 'password1234';
     data.old_password = 'password1234';
-    let output = validateInput(data);
+    const output = validateInput(data);
     expect(output.errors.password).to.equal('old password same as new password');
   });
- 
 });
