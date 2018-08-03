20c7eeed0c8edcf12412d97b84962695e591cb00
victorjambo <victorjambo@live.com>
2018-08-04 00:20:47 +0300

    [Fixes] superagent

--- src/helpers/superagent.js
+++ src/helpers/superagent.js
@@ -1,7 +1,7 @@
-// import superagentAbsolute from 'superagent-absolute';
+import superagentAbsolute from 'superagent-absolute';
 import request from 'superagent';
 
 const agent = request.agent();
-const requestAgent = process.env.REACT_APP_BASE_URL;
+const requestAgent = superagentAbsolute(agent)(process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_VERSION);
 
 export default requestAgent;
