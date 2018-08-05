1b1fd41fec083ab3af1e5f0360fea3f8933b0b1e
victorjambo <victorjambo@live.com>
2018-08-05 17:30:33 +0300

    [chore] remove superagentAbsolute module

---
+++ src/helpers/superagent-absolute.js
@@ -0,0 +1,20 @@
+const OVERRIDE = 'delete,get,head,patch,post,put'.split(',');
+
+const superagentAbsolute = agent => baseUrl => (
+  new Proxy(agent, {
+    get(target, propertyName) {
+      return (...params) => {
+        if (OVERRIDE.indexOf(propertyName) !== -1 &&
+          params.length > 0 &&
+          typeof params[0] === 'string' &&
+          params[0].startsWith('/')) {
+          const absoluteUrl = baseUrl + params[0];
+          return target[propertyName](absoluteUrl, ...params.slice(1));
+        }
+        return target[propertyName](...params);
+      };
+    },
+  })
+);
+
+export default superagentAbsolute;
--- src/helpers/superagent.js
+++ src/helpers/superagent.js
@@ -1,5 +1,5 @@
-import superagentAbsolute from 'superagent-absolute';
 import request from 'superagent';
+import superagentAbsolute from './superagent-absolute';
 
 const agent = request.agent();
 const requestAgent = superagentAbsolute(agent)(process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_VERSION);
